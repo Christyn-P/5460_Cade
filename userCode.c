de7497ed7f92d6d7a46a38729939c3e166b340c9
Christyn-P <naniganz@gmail.com>
2017-04-07 05:33:24 -0600

    create usercode file

---
+++ userCode.c
@@ -0,0 +1,22 @@
+
+#include <linux/version.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/mutex.h>
+
+#include <asm/uaccess.h>
+
+int main(void){
+
+
+return 0;
+}
